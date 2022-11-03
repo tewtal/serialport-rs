e371b969f68eb3177b4f8d5f2fc3c97f7d6efa0a
Thomas Backmark <total@viskos.org>
2022-11-03 23:08:37 +0100

    Update com.rs

--- src/windows/com.rs
+++ src/windows/com.rs
@@ -247,8 +247,8 @@ impl SerialPort for COMPort {
             // return as soon as bytes become available (like POSIX would) and
             // block up to given duration otherwise
             // https://docs.microsoft.com/en-us/windows/win32/api/winbase/ns-winbase-commtimeouts#remarks
-            ReadIntervalTimeout: 1,
-            ReadTotalTimeoutMultiplier: 0,
+            ReadIntervalTimeout: MAXDWORD,
+            ReadTotalTimeoutMultiplier: MAXDWORD,
             ReadTotalTimeoutConstant: milliseconds as DWORD,
             // block without timeout until write is complete
             // MAXDWORD is *not* a reserved WriteTotalTimeoutMultiplier
